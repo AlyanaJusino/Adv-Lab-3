ff6c106eb12f41f9f82703f85d94faab3ac7f2a6
AlyanaJusino <alyanajusino@gmail.com>
2025-10-27 10:25:45 -0400

    Update phys_lab_3.ino

--- phys_lab_3.ino
+++ phys_lab_3.ino
@@ -1,42 +1,26 @@
 void setup() {
-   
   
   Serial.begin(9600);
 
-  // The heater resistor will be controlled by a PWM pin.
   pinMode(9, OUTPUT);
-}
-
-// The loop() function runs over and over so whatever is contained
-// therein will be repeated while the Arduino is on. 
+} 
 
 void loop() {
-  
-  // The function analogRead() will
-  // read the input on analog pins:
-  // A0 for the thermistor, A1 for the potentiometer
   
   int thermValue = analogRead(A0);
   int potValue = analogRead(A1);
   
-  // Map potentiometer reading (0–1023) to PWM range (0–255)
-  
   int heaterPower = map(potValue, 0, 1023, 0, 255);
   
-  // Write the PWM signal to the heater control pin
-  
   analogWrite(9, heaterPower);
-  
-  // Add time to the serial output and print values separated by commas:
+
   // time, potentiometer, thermistor
-  
   Serial.print(millis());
   Serial.print(',');
   Serial.print(potValue);
   Serial.print(',');
   Serial.println(thermValue);
   
-  // Delay for 200 milliseconds between each loop
-  
+  // Delay 
   delay(200);
 }
